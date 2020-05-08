690eb92f161900e02c3e071166355f84986bb03b
Daniel Merys <dkmerys@gmail.com>
2020-05-08 08:44:29 -0700

    run Gem bundle

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,1 +1,9 @@
-*
+require ('rspec')
+require ('anagrams')
+
+describe ('Word_Checker') do
+  it ("accepts user input") do
+    word_checker = Word_Checker.new("Hello")
+    expect(word_checker.user_word_1).to(eq("Hello"))
+  end
+end
